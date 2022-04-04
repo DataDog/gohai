928795eb7349ba3cabf8f7fc5c623cf814aae310
Julien Lebot <julien.lebot@datadoghq.com>
2022-04-04 14:09:51 +0200

    Add Windows-specific code

--- gohai_test.go
+++ gohai_test.go
@@ -28,13 +28,22 @@ type gohaiPayload struct {
 		ModelName            string `json:"model_name"`
 		Stepping             string `json:"stepping"`
 		VendorID             string `json:"vendor_id"`
+
+		// On Windows, we report additional fields
+		CacheSizeL1  string `json:"cache_size_l1"`
+		CacheSizeL2  string `json:"cache_size_l2"`
+		CacheSizeL3  string `json:"cache_size_l3"`
+		CPUNumaNodes string `json:"cpu_numa_nodes"`
+		CPUPkgs      string `json:"cpu_pkgs"`
 	} `json:"cpu"`
 	Filesystem []struct {
-		KbSize    string `json:"kb_size"`
+		KbSize string `json:"kb_size"`
+		// MountedOn can be empty on Windows
 		MountedOn string `json:"mounted_on"`
 		Name      string `json:"name"`
 	} `json:"filesystem"`
 	Memory struct {
+		// SwapTotal is not reported on Windows
 		SwapTotal string `json:"swap_total"`
 		Total     string `json:"total"`
 	} `json:"memory"`
@@ -58,10 +67,13 @@ type gohaiPayload struct {
 		Hostname      string `json:"hostname"`
 		KernelName    string `json:"kernel_name"`
 		KernelRelease string `json:"kernel_release"`
+		// KernelVersion is not reported on Windows
 		KernelVersion string `json:"kernel_version"`
 		Machine       string `json:"machine"`
 		Os            string `json:"os"`
 		Processor     string `json:"processor"`
+		// On Windows, we report additional fields
+		Family string `json:"family"`
 	} `json:"platform"`
 }
 
@@ -85,14 +97,25 @@ func TestGohaiSerialization(t *testing.T) {
 	assert.NotEmpty(t, payload.CPU.Stepping)
 	assert.NotEmpty(t, payload.CPU.VendorID)
 
+	if runtime.GOOS == "windows" {
+		// Additional fields that we report on Windows
+		assert.NotEmpty(t, payload.CPU.CacheSizeL1)
+		assert.NotEmpty(t, payload.CPU.CacheSizeL2)
+		assert.NotEmpty(t, payload.CPU.CacheSizeL3)
+		assert.NotEmpty(t, payload.CPU.CPUNumaNodes)
+		assert.NotEmpty(t, payload.CPU.CPUPkgs)
+	}
+
 	if assert.NotEmpty(t, payload.Filesystem) {
 		if runtime.GOOS != "windows" {
-			assert.NotEmpty(t, payload.Filesystem[0].KbSize, 0)
+			// On Windows, MountedOn can be empty
+			assert.NotEmpty(t, payload.Filesystem[0].MountedOn, 0)
 		}
-		assert.NotEmpty(t, payload.Filesystem[0].MountedOn, 0)
+		assert.NotEmpty(t, payload.Filesystem[0].KbSize, 0)
 		assert.NotEmpty(t, payload.Filesystem[0].Name, 0)
 	}
 	if runtime.GOOS != "windows" {
+		// Not reported on Windows
 		assert.NotEmpty(t, payload.Memory.SwapTotal)
 	}
 	assert.NotEmpty(t, payload.Memory.Total)
@@ -118,12 +141,14 @@ func TestGohaiSerialization(t *testing.T) {
 	assert.NotEmpty(t, payload.Platform.Hostname)
 	assert.NotEmpty(t, payload.Platform.KernelName)
 	assert.NotEmpty(t, payload.Platform.KernelRelease)
-	if runtime.GOOS != "windows" {
-		assert.NotEmpty(t, payload.Platform.KernelVersion)
-	}
 	assert.NotEmpty(t, payload.Platform.Machine)
 	assert.NotEmpty(t, payload.Platform.Os)
 	if runtime.GOOS != "windows" {
+		// Not reported on Windows
+		assert.NotEmpty(t, payload.Platform.KernelVersion)
 		assert.NotEmpty(t, payload.Platform.Processor)
+	} else {
+		// Additional fields that we report on Windows
+		assert.NotEmpty(t, payload.Platform.Family)
 	}
 }
