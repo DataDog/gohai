a9ab59a8a7c6db39bdc7ec152cd5dfad2d4cc824
Julien Lebot <julien.lebot@datadoghq.com>
2022-04-04 11:19:47 +0200

    Update indices

--- filesystem/filesystem_darwin.go
+++ filesystem/filesystem_darwin.go
@@ -7,6 +7,6 @@ func updatefileSystemInfo(values []string) map[string]string {
 	return map[string]string{
 		"name":       values[0],
 		"kb_size":    values[1],
-		"mounted_on": values[8],
+		"mounted_on": values[5],
 	}
 }
