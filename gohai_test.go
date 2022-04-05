15170ad368b4f75a8d96dddb551d2b3da78409d5
Julien Lebot <julien.lebot@datadoghq.com>
2022-04-05 12:58:36 +0200

    Improve test for network addresses

--- gohai_test.go
+++ gohai_test.go
@@ -129,14 +129,17 @@ func TestGohaiSerialization(t *testing.T) {
 	if assert.NotEmpty(t, payload.Network.Interfaces) {
 		for _, itf := range payload.Network.Interfaces {
 			assert.NotEmpty(t, itf.Name)
-			assert.NotEmpty(t, itf.Macaddress)
-			if len(itf.Ipv4) == 0 {
+			// Some interfaces don't have MacAddresses
+			//assert.NotEmpty(t, itf.Macaddress)
+			// Disabled interfaces won't have any IP address
+			if len(itf.Ipv6) > 0 {
 				assert.NotEmpty(t, itf.Ipv6)
 				assert.NotEmpty(t, itf.Ipv6Network)
 				for _, ip := range itf.Ipv6 {
 					assert.NotNil(t, net.ParseIP(ip))
 				}
-			} else {
+			}
+			if len(itf.Ipv4) > 0 {
 				assert.NotEmpty(t, itf.Ipv4)
 				assert.NotEmpty(t, itf.Ipv4Network)
 				for _, ip := range itf.Ipv4 {
